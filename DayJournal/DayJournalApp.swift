96d892ff06abcef85f189775f0a4c93ef4d5e0f0
samisamara <samisamara99@yahoo.com>
2024-03-25 17:57:27 -0500

    Initial Commit

---
+++ DayJournal/DayJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayJournalApp.swift
+//  DayJournal
+//
+//  Created by Sami Samara on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DayJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
